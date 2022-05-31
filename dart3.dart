20e69b92db8b07e95d5a3ed693c65e0fc6060360
Tshepocodes <106625935+Tshepocodes@users.noreply.github.com>
2022-06-01 00:30:13 +0200

    Add files via upload

--- dart3.dart
+++ dart3.dart
@@ -1,10 +1,36 @@
 void main() {
-// developer, and the year it won MTN Business App of the Year Awards.
+//Create a class and a) then use an object to print the name of the app,
+//sector/category,developer, and the year it won MTN Business App of the Year Awards.
 //
+
+  var name = Name();
+  print(name.nameOfTheApp);
+  print(name.sector);
+  print(name.category);
+  print(name.yearItWon);
+  
 }
 
-//Create a class and a) then use an object to print the name of the app,
-//sector/category,
-class Name {}
+class Name {
+  var nameOfTheApp = 'Fnb App';
+  var sector = 'Banking';
+  var category = 'Best Financial Solution';
+  var yearItWon = '2012';
+
+//Reference to the classess
+
+  Name() {
+    this.nameOfTheApp = nameOfTheApp;
+    this.sector = sector;
+    this.category = category;
+    this.yearItWon = yearItWon;
+  }
 // b) Create a function inside the class, transform the app name to all capital letters
 // and then print the output.
+void letterTransform(String value) {
+    var upper = value.toUpperCase();
+    print(upper);
+  }
+
+}
+  
